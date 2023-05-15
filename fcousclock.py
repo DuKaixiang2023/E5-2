94e266dba8942f29bfe58600f9e80cf8167e2748
DuKaixiang2023 <133679908+DuKaixiang2023@users.noreply.github.com>
2023-05-16 00:04:37 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,13 @@
+import time
+
+minutes = int(input("Enter time"))
+total_seconds = minutes * 60
+
+while total_seconds:
+    mins, secs = divmod(total_seconds, 60)
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    print(timer, end="\r")
+    time.sleep(1)
+    total_seconds -= 1
+    
+print("Focus timer complete!")
