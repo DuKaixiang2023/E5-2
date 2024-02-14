d5f0951596c9b272aaa0412be3f0e2b7e26e490e
DuKaixiang2023 <133679908+DuKaixiang2023@users.noreply.github.com>
2024-02-14 12:56:17 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -5,6 +5,7 @@ import time
 minutes = int(input("Enter time"))
 total_seconds = minutes * 60
 
+
 while total_seconds:
     mins, secs = divmod(total_seconds, 60)
     timer = '{:02d}:{:02d}'.format(mins, secs)
